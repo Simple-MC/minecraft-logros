d5f530fd4d0978105f7dd8e8745ab2cddbe767d0
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 11:31:25 -0800

    Crear profundo.js

---
+++ logros/profundo.js
@@ -0,0 +1,13 @@
+window.logroData = {
+    name: "A lo profundo",
+    icono: "fa-fire",
+    categoria: "Nether",
+    inventoryNed: ["10x Obsidiana", "Mechero (Pedernal y hierro)"],
+    objetosOpcionales: ["Armadura de Oro (para los Piglins)", "Pico de Diamante"],
+    tutorial: [
+        "Construye un marco de 4x5 con bloques de Obsidiana.",
+        "Usa el mechero para encender el interior del marco.",
+        "Cruza el portal morado y espera a que cargue la dimensión."
+    ],
+    proTip: "Lleva siempre un bloque de oro puesto para que los Piglins no te ataquen apenas entres."
+};
