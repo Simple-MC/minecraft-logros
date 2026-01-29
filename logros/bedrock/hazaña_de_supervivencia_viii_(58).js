5a6797de65f52fa2f588f9eb0d22ae63f2b7b077
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:23 -0800

    Auto-Wiki: Hazaña de Supervivencia VIII (58) (bedrock)

---
+++ logros/bedrock/hazaña_de_supervivencia_viii_(58).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Hazaña de Supervivencia VIII (58)",
+    "icono": "zap",
+    "categoria": "Progresión",
+    "inventoryNed": [
+        "experience_bottle"
+    ],
+    "tutorial": [
+        "Alcanza el nivel 40 de experiencia"
+    ],
+    "proTip": "Comerciar con un aldeano clérigo por ojos de ender es eficiente.",
+    "edition": "bedrock"
+};
