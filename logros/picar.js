d7045c484b0212601adff87ca0bd4dff2d2754d1
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 18:02:35 -0800

    Build: picar.js

---
+++ logros/picar.js
@@ -0,0 +1,8 @@
+registrarLogro({
+    id: "inicio_basico",
+    titulo: "Picar la Madera",
+    descripcion: "Consigue el primer tronco de cualquier árbol y utilízalo para craftear tu primera mesa de trabajo.",
+    tipo: "Crafteo/Exploración",
+    requisitos: "Picar 1 tronco.",
+    ediciones: ["java", "bedrock"]
+});
