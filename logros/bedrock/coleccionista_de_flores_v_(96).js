094defab0f5c42b4ac98f02a99f6d82938e88bb5
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:44 -0800

    Auto-Wiki: Coleccionista de Flores V (96) (bedrock)

---
+++ logros/bedrock/coleccionista_de_flores_v_(96).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Coleccionista de Flores V (96)",
+    "icono": "flower",
+    "categoria": "Estética",
+    "inventoryNed": [
+        "lily_of_the_valley"
+    ],
+    "tutorial": [
+        "Recolecta un Lirio del Valle"
+    ],
+    "proTip": "Se encuentra en bosques y biomas de taiga.",
+    "edition": "bedrock"
+};
