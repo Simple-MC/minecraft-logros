5ef7deab6878f7f9cc9836f64e515a6189922133
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:19 -0800

    Auto-Wiki: Hazaña de Supervivencia I (51) (bedrock)

---
+++ logros/bedrock/hazaña_de_supervivencia_i_(51).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Hazaña de Supervivencia I (51)",
+    "icono": "zap",
+    "categoria": "Progresión",
+    "inventoryNed": [
+        "experience_bottle"
+    ],
+    "tutorial": [
+        "Alcanza el nivel 5 de experiencia"
+    ],
+    "proTip": "Matar mobs y fundir recursos son las mejores fuentes de XP temprana.",
+    "edition": "bedrock"
+};
