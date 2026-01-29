ddbdb10fff266136bb9134a259b09ed4c72e1699
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:48 -0800

    Auto-Wiki: El Pescador I (102) (bedrock)

---
+++ logros/bedrock/el_pescador_i_(102).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "El Pescador I (102)",
+    "icono": "fishing-rod",
+    "categoria": "Pesca",
+    "inventoryNed": [
+        "cod"
+    ],
+    "tutorial": [
+        "Pesca 10 peces Crudos"
+    ],
+    "proTip": "La mejor caña tiene Suerte del Mar III y Atracción III.",
+    "edition": "bedrock"
+};
