728476fb02167d0f66d86bca728d7cd502cfed68
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:38 -0800

    Auto-Wiki: Enemigo de los Esqueletos (85) (bedrock)

---
+++ logros/bedrock/enemigo_de_los_esqueletos_(85).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Enemigo de los Esqueletos (85)",
+    "icono": "bone",
+    "categoria": "Combate",
+    "inventoryNed": [
+        "bow"
+    ],
+    "tutorial": [
+        "Mata 100 Esqueletos"
+    ],
+    "proTip": "Los escudos bloquean las flechas.",
+    "edition": "bedrock"
+};
