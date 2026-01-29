6ad922f34124c27d4f96f7d292d878e765002b68
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:36 -0800

    Auto-Wiki: Maestro Minero VI (81) (bedrock)

---
+++ logros/bedrock/maestro_minero_vi_(81).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Maestro Minero VI (81)",
+    "icono": "pickaxe",
+    "categoria": "Minería",
+    "inventoryNed": [
+        "diamond"
+    ],
+    "tutorial": [
+        "Mina 50 minerales de Diamante"
+    ],
+    "proTip": "Mina en franjas largas en el nivel Y=-59 para obtener la mayor eficiencia.",
+    "edition": "bedrock"
+};
