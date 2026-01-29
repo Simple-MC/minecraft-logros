66e939b84c5ee2f2a8b9bd409d6813dfc4b2001f
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:09 -0800

    Auto-Wiki: La Segunda Ciudad (35) (bedrock)

---
+++ logros/bedrock/la_segunda_ciudad_(35).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "La Segunda Ciudad (35)",
+    "icono": "air-vent",
+    "categoria": "Exploración",
+    "inventoryNed": [
+        "elytra"
+    ],
+    "tutorial": [
+        "Entra a las Ciudades del End a través de los portales pequeños después de vencer al Dragón"
+    ],
+    "proTip": "Busca la nave y obtén las Elytras (alas).",
+    "edition": "bedrock"
+};
