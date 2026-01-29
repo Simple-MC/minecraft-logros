1c2dfab2271617ab73824234e7f9353d926ad5df
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:21 -0800

    Auto-Wiki: Hazaña de Supervivencia IV (54) (bedrock)

---
+++ logros/bedrock/hazaña_de_supervivencia_iv_(54).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Hazaña de Supervivencia IV (54)",
+    "icono": "zap",
+    "categoria": "Progresión",
+    "inventoryNed": [
+        "experience_bottle"
+    ],
+    "tutorial": [
+        "Alcanza el nivel 20 de experiencia"
+    ],
+    "proTip": "Busca una granja de Guardianes o Blazes para XP rápido.",
+    "edition": "bedrock"
+};
