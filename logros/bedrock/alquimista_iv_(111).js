d3387fa3924ea34b278a89f50d909f5558b51e0f
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:52 -0800

    Auto-Wiki: Alquimista IV (111) (bedrock)

---
+++ logros/bedrock/alquimista_iv_(111).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Alquimista IV (111)",
+    "icono": "vial",
+    "categoria": "Pociones",
+    "inventoryNed": [
+        "spider_eye"
+    ],
+    "tutorial": [
+        "Crea una poción de Veneno"
+    ],
+    "proTip": "Se convierte en poción de Cura con un ojo de araña fermentado.",
+    "edition": "bedrock"
+};
