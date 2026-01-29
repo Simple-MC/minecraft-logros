3dbd27aefa02824d906395029ca621066b9bae95
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 18:02:36 -0800

    Build: dragon.js

---
+++ logros/dragon.js
@@ -0,0 +1,8 @@
+registrarLogro({
+    id: "derrota_dragon",
+    titulo: "¿Libre Al Fin?",
+    descripcion: "Derrota al Dragón del End y presencia los créditos finales.",
+    tipo: "Jefe Final",
+    requisitos: "Entrar al End y matar al Dragón.",
+    ediciones: ["java", "bedrock"]
+});
