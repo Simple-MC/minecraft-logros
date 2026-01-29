26bdd5b41df28686e3ed828cb952a7827d28d934
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:24:11 -0800

    Auto-Wiki: Mejorando la Armadura (bedrock)

---
+++ logros/bedrock/mejorando_la_armadura.js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Mejorando la Armadura",
+    "icono": "shield",
+    "categoria": "Combate y Defensa",
+    "inventoryNed": [
+        "Lingotes de Hierro (mínimo 24 para un set completo)"
+    ],
+    "tutorial": [
+        "1. Craftea cualquier pieza de armadura (botas, casco, peto o leggings) usando Lingotes de Hierro en la Mesa de Crafteo."
+    ],
+    "proTip": "La armadura de hierro ofrece una defensa decente, siendo el peto la pieza que más protección otorga.",
+    "edition": "bedrock"
+};
