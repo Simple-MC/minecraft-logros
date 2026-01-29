d8325e81d9b1d1a208ad8727cb6f316f3417545d
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 18:02:38 -0800

    Build: nether.js

---
+++ logros/nether.js
@@ -0,0 +1,8 @@
+registrarLogro({
+    id: "dimension_roja",
+    titulo: "¡Necesitamos Profundizar!",
+    descripcion: "Construye y atraviesa un portal de Obsidiana al Nether.",
+    tipo: "Exploración/Dimensional",
+    requisitos: "Encender un portal al Nether y cruzarlo.",
+    ediciones: ["java", "bedrock"]
+});
