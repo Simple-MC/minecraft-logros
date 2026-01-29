500aa95a785a9068cba40dd4141e50e082846cad
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:57 -0800

    Auto-Wiki: El Iluminador (119) (bedrock)

---
+++ logros/bedrock/el_iluminador_(119).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "El Iluminador (119)",
+    "icono": "lightbulb",
+    "categoria": "Construcción",
+    "inventoryNed": [
+        "glowstone"
+    ],
+    "tutorial": [
+        "Coloca 500 fuentes de luz (Antorchas, Glowstone, etc.)"
+    ],
+    "proTip": "La iluminación evita que aparezcan mobs.",
+    "edition": "bedrock"
+};
