b8bb95380944d129b37f9fda545549cce4da2ea6
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:13 -0800

    Auto-Wiki: Amigos Peludos (42) (bedrock)

---
+++ logros/bedrock/amigos_peludos_(42).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Amigos Peludos (42)",
+    "icono": "bone",
+    "categoria": "Mascotas",
+    "inventoryNed": [
+        "bone"
+    ],
+    "tutorial": [
+        "Domestica un lobo con huesos"
+    ],
+    "proTip": "Los lobos te defenderán de los mobs hostiles.",
+    "edition": "bedrock"
+};
