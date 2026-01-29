4559c90872e60a485ce096587e42ec922af746dc
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:26 -0800

    Auto-Wiki: Explorador de Biomas (63) (bedrock)

---
+++ logros/bedrock/explorador_de_biomas_(63).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Explorador de Biomas (63)",
+    "icono": "tree-pine",
+    "categoria": "Viaje",
+    "inventoryNed": [
+        "map"
+    ],
+    "tutorial": [
+        "Descubre un bioma de Savanna"
+    ],
+    "proTip": "La madera de acacia es distintiva y útil.",
+    "edition": "bedrock"
+};
