6258674a46bf691b7b21ebf044de87be3a844dc3
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:55 -0800

    Auto-Wiki: El Golem de Nieve (115) (bedrock)

---
+++ logros/bedrock/el_golem_de_nieve_(115).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "El Golem de Nieve (115)",
+    "icono": "snowflake",
+    "categoria": "Defensa",
+    "inventoryNed": [
+        "snow_block"
+    ],
+    "tutorial": [
+        "Crea un Golem de Nieve"
+    ],
+    "proTip": "Se derriten en biomas cálidos y dejan un rastro de nieve.",
+    "edition": "bedrock"
+};
