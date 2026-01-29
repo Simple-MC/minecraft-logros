4f66c1de1613a8e234ba3a9b46f432c2ab41f793
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:40 -0800

    Auto-Wiki: Enemigo de los Phantoms (88) (bedrock)

---
+++ logros/bedrock/enemigo_de_los_phantoms_(88).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Enemigo de los Phantoms (88)",
+    "icono": "wind",
+    "categoria": "Combate",
+    "inventoryNed": [
+        "phantom_membrane"
+    ],
+    "tutorial": [
+        "Mata 50 Phantoms"
+    ],
+    "proTip": "Solo aparecen si no has dormido en 3 días.",
+    "edition": "bedrock"
+};
