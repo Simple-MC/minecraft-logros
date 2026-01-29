821134ea25dc2732439d55bb49efdbfe8d3c0565
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:40 -0800

    Auto-Wiki: Enemigo de los Hoglins (89) (bedrock)

---
+++ logros/bedrock/enemigo_de_los_hoglins_(89).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Enemigo de los Hoglins (89)",
+    "icono": "pig",
+    "categoria": "Nether",
+    "inventoryNed": [
+        "porkchop"
+    ],
+    "tutorial": [
+        "Mata 50 Hoglins en el Nether"
+    ],
+    "proTip": "Se asustan por el olor de los hongos distorsionados.",
+    "edition": "bedrock"
+};
