3f72512e2b84f4310f5471de168ea9d35103ff50
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:58 -0800

    Auto-Wiki: El Encantador I (120) (bedrock)

---
+++ logros/bedrock/el_encantador_i_(120).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "El Encantador I (120)",
+    "icono": "book-open-text",
+    "categoria": "Encantamientos",
+    "inventoryNed": [
+        "enchanting_table"
+    ],
+    "tutorial": [
+        "Encanta una herramienta en el nivel I"
+    ],
+    "proTip": "Necesitas 15 librerías para obtener los encantamientos máximos.",
+    "edition": "bedrock"
+};
