7eed718cb05a6910f9764c90953b4e1793093fa1
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:51 -0800

    Auto-Wiki: Alquimista I (108) (bedrock)

---
+++ logros/bedrock/alquimista_i_(108).js
@@ -0,0 +1,14 @@
+window.logroData = {
+    "name": "Alquimista I (108)",
+    "icono": "vial",
+    "categoria": "Pociones",
+    "inventoryNed": [
+        "brewing_stand",
+        "water_bottle"
+    ],
+    "tutorial": [
+        "Crea una poción de Visión Nocturna"
+    ],
+    "proTip": "La base de todas las pociones es la Verruga del Nether (Nether Wart).",
+    "edition": "bedrock"
+};
