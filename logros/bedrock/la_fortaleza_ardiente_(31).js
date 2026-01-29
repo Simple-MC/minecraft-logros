22e42419f7f9614a64fa7518daafa6e265b73278
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:07 -0800

    Auto-Wiki: La Fortaleza Ardiente (31) (bedrock)

---
+++ logros/bedrock/la_fortaleza_ardiente_(31).js
@@ -0,0 +1,14 @@
+window.logroData = {
+    "name": "La Fortaleza Ardiente (31)",
+    "icono": "skull",
+    "categoria": "Nether",
+    "inventoryNed": [
+        "blaze_rod"
+    ],
+    "tutorial": [
+        "Encuentra una Fortaleza del Nether",
+        "Derrota a un Blaze para obtener la Vara de Blaze"
+    ],
+    "proTip": "El fuego es tu enemigo en la Fortaleza. Lleva pociones o bloques.",
+    "edition": "bedrock"
+};
