116c45b7f53bf8189ef6a1d1e1fd60155fe51a7d
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:03 -0800

    Auto-Wiki: Obteniendo Hierro (23) (bedrock)

---
+++ logros/bedrock/obteniendo_hierro_(23).js
@@ -0,0 +1,16 @@
+window.logroData = {
+    "name": "Obteniendo Hierro (23)",
+    "icono": "ingot",
+    "categoria": "Minería",
+    "inventoryNed": [
+        "raw_iron",
+        "furnace"
+    ],
+    "tutorial": [
+        "Desciende a la cueva",
+        "Mina mineral de hierro",
+        "Funde el hierro en el horno"
+    ],
+    "proTip": "Busca hierro cerca del nivel Y=30.",
+    "edition": "bedrock"
+};
