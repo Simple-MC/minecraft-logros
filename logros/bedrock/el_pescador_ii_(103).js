0ad5baffe7236d156dea812e20490603cc5adee0
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:48 -0800

    Auto-Wiki: El Pescador II (103) (bedrock)

---
+++ logros/bedrock/el_pescador_ii_(103).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "El Pescador II (103)",
+    "icono": "fishing-rod",
+    "categoria": "Pesca",
+    "inventoryNed": [
+        "salmon"
+    ],
+    "tutorial": [
+        "Pesca 50 peces Crudos"
+    ],
+    "proTip": "Pescar en la lluvia aumenta la velocidad.",
+    "edition": "bedrock"
+};
