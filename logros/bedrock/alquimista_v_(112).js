e94c07e259295d8ed67dfa764ee46c6ffd50fa5b
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:53 -0800

    Auto-Wiki: Alquimista V (112) (bedrock)

---
+++ logros/bedrock/alquimista_v_(112).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Alquimista V (112)",
+    "icono": "vial",
+    "categoria": "Pociones",
+    "inventoryNed": [
+        "sugar"
+    ],
+    "tutorial": [
+        "Crea una poción de Velocidad"
+    ],
+    "proTip": "Requiere azúcar.",
+    "edition": "bedrock"
+};
