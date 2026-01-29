04a81095848f2e2d8f2ce2bac6d0fe894b37db11
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:16 -0800

    Auto-Wiki: La Estructura Submarina (46) (bedrock)

---
+++ logros/bedrock/la_estructura_submarina_(46).js
@@ -0,0 +1,14 @@
+window.logroData = {
+    "name": "La Estructura Submarina (46)",
+    "icono": "fish",
+    "categoria": "Exploración",
+    "inventoryNed": [
+        "sponge"
+    ],
+    "tutorial": [
+        "Encuentra un Monumento Oceánico",
+        "Derrota a los Guardianes Ancianos y obtén una Esponja Húmeda"
+    ],
+    "proTip": "Usa pociones de Respiración Acuática y Visión Nocturna.",
+    "edition": "bedrock"
+};
