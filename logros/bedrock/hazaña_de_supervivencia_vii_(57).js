ec4a58c67ce36069ff9e62079c6a0518876bc3f4
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:22 -0800

    Auto-Wiki: Hazaña de Supervivencia VII (57) (bedrock)

---
+++ logros/bedrock/hazaña_de_supervivencia_vii_(57).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Hazaña de Supervivencia VII (57)",
+    "icono": "zap",
+    "categoria": "Progresión",
+    "inventoryNed": [
+        "experience_bottle"
+    ],
+    "tutorial": [
+        "Alcanza el nivel 35 de experiencia"
+    ],
+    "proTip": "Los cofres del tesoro enterrados a menudo contienen experiencia.",
+    "edition": "bedrock"
+};
