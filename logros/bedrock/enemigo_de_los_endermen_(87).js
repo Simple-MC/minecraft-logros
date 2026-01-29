6be4871e896f7877ee19a78cb1602cf19a88ddbf
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:39 -0800

    Auto-Wiki: Enemigo de los Endermen (87) (bedrock)

---
+++ logros/bedrock/enemigo_de_los_endermen_(87).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Enemigo de los Endermen (87)",
+    "icono": "eye",
+    "categoria": "Combate",
+    "inventoryNed": [
+        "ender_pearl"
+    ],
+    "tutorial": [
+        "Mata 50 Endermen"
+    ],
+    "proTip": "Mira sus piernas para atacarlos sin que te teletransporten.",
+    "edition": "bedrock"
+};
