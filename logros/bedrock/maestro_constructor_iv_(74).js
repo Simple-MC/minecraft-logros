8cb09b31faed603336ba83b10decc002d1afb1a4
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:32 -0800

    Auto-Wiki: Maestro Constructor IV (74) (bedrock)

---
+++ logros/bedrock/maestro_constructor_iv_(74).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Maestro Constructor IV (74)",
+    "icono": "house",
+    "categoria": "Construcción",
+    "inventoryNed": [
+        "concrete"
+    ],
+    "tutorial": [
+        "Coloca 1,000 bloques de hormigón"
+    ],
+    "proTip": "El hormigón se fabrica mezclando arena, grava y tinte con agua.",
+    "edition": "bedrock"
+};
