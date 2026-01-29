157596bac8d9245c2b95ec0f7d9f3b96dc716b00
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:34 -0800

    Auto-Wiki: Maestro Minero III (78) (bedrock)

---
+++ logros/bedrock/maestro_minero_iii_(78).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Maestro Minero III (78)",
+    "icono": "pickaxe",
+    "categoria": "Minería",
+    "inventoryNed": [
+        "lapis_lazuli"
+    ],
+    "tutorial": [
+        "Mina 200 minerales de Lapis Lazuli"
+    ],
+    "proTip": "Esencial para los primeros encantamientos.",
+    "edition": "bedrock"
+};
