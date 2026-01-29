37056cb952e9ff25f9a91214cd808ca60aee0fb2
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:56 -0800

    Auto-Wiki: Almacenamiento Infinito (117) (bedrock)

---
+++ logros/bedrock/almacenamiento_infinito_(117).js
@@ -0,0 +1,14 @@
+window.logroData = {
+    "name": "Almacenamiento Infinito (117)",
+    "icono": "warehouse",
+    "categoria": "Automatización",
+    "inventoryNed": [
+        "hopper",
+        "chest"
+    ],
+    "tutorial": [
+        "Crea un sistema de almacenamiento masivo con tolvas"
+    ],
+    "proTip": "Las tolvas permiten transferir ítems entre cofres y máquinas.",
+    "edition": "bedrock"
+};
