32efab80b61a1dd115f1b244acfcbb2144eef5f9
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:49 -0800

    Auto-Wiki: El Arquero (105) (bedrock)

---
+++ logros/bedrock/el_arquero_(105).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "El Arquero (105)",
+    "icono": "arrow",
+    "categoria": "Combate",
+    "inventoryNed": [
+        "bow"
+    ],
+    "tutorial": [
+        "Mata a 50 mobs hostiles usando flechas"
+    ],
+    "proTip": "El encantamiento Poder V es devastador.",
+    "edition": "bedrock"
+};
