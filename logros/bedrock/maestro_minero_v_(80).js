ef391b5892e5399f139bba2d3db2d448d4439255
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:35 -0800

    Auto-Wiki: Maestro Minero V (80) (bedrock)

---
+++ logros/bedrock/maestro_minero_v_(80).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Maestro Minero V (80)",
+    "icono": "pickaxe",
+    "categoria": "Minería",
+    "inventoryNed": [
+        "raw_gold"
+    ],
+    "tutorial": [
+        "Mina 100 minerales de Oro"
+    ],
+    "proTip": "El oro es más común en el Nether.",
+    "edition": "bedrock"
+};
