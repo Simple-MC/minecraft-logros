8eea52aab37c835892aeb6e9a966954061398bd0
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-29 19:02:42 -0800

    Clean

--- logros/picar.js
+++
@@ -1,9 +0,0 @@
-/** Este archivo simula la estructura de un archivo de detalle que se cargaría para una vista de logro individual. */
-window.logroData = {
-    id: 'mine_wood',
-    name: 'Talando árboles',
-    desc: 'El primer paso en la supervivencia. Consigue tu primer tronco de madera.',
-    type: 'Java/Bedrock',
-    prerequisito: null,
-    guia: 'Busca cualquier árbol y utiliza la mano para golpearlo hasta que se rompa. Es lento, ¡pero funciona!'
-};
