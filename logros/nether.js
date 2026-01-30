3051b15256ce845c3da43600bc8269c547ec1fbf
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-29 19:02:41 -0800

    Clean

--- logros/nether.js
+++
@@ -1,9 +0,0 @@
-/** Este archivo simula la estructura de un archivo de detalle que se cargaría para una vista de logro individual. */
-window.logroData = {
-    id: 'un_viaje_obligado',
-    name: 'Un viaje obligado',
-    desc: 'Entra a la dimensión del Nether. ¡Ten cuidado!',
-    type: 'Java/Bedrock',
-    prerequisito: 'Obsidiana y pedernal',
-    guia: 'Necesitas 14 bloques de Obsidiana para construir el marco y encenderlo con un mechero (pedernal y hierro).'
-};
